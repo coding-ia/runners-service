8e489082c5d4927c0edf3b034943e0c7317d66a2
bjunker <brian.junker@gmail.com>
2024-10-14 09:47:03 -0700

    Development branch

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	sdk "github.com/firecracker-microvm/firecracker-go-sdk"
+	"github.com/firecracker-microvm/firecracker-go-sdk/client/models"
+	"log"
+	"os"
+)
+
+func createNewConfig(socketPath string) sdk.Config {
+	dir, _ := os.Getwd()
+	fmt.Println(dir)
+	kernelImage := "/home/brian/custom/vmlinux-5.10.223"
+
+	var vcpuCount int64 = 1
+	var memSizeMib int64 = 1024
+	smt := false
+
+	driveID := "root"
+	isRootDevice := true
+	isReadOnly := false
+	pathOnHost := "/home/brian/custom/ubuntu-22.04.ext4"
+
+	cfg := sdk.Config{
+		SocketPath:      socketPath,
+		KernelArgs:      "console=ttyS0 loglevel=3 reboot=k panic=1 pci=off selinux=0",
+		KernelImagePath: kernelImage,
+		MachineCfg: models.MachineConfiguration{
+			VcpuCount:  &vcpuCount,
+			MemSizeMib: &memSizeMib,
+			Smt:        &smt,
+		},
+		Drives: []models.Drive{
+			{
+				DriveID:      &driveID,
+				IsRootDevice: &isRootDevice,
+				IsReadOnly:   &isReadOnly,
+				PathOnHost:   &pathOnHost,
+			},
+		},
+	}
+
+	return cfg
+}
+
+func main() {
+	fcSocket := "/tmp/firecracker.socket"
+	ctx := context.TODO()
+
+	networkInterface := sdk.NetworkInterface{
+		CNIConfiguration: &sdk.CNIConfiguration{
+			NetworkName: "fcnet",
+			IfName:      "veth0",
+			VMIfName:    "eth0",
+		},
+	}
+
+	vmConfig := createNewConfig(fcSocket)
+	vmConfig.NetworkInterfaces = append(vmConfig.NetworkInterfaces, networkInterface)
+
+	cmd := sdk.VMCommandBuilder{}.WithSocketPath(fcSocket).WithBin("/home/brian/firecracker/firecracker").Build(ctx)
+	m, err := sdk.NewMachine(ctx, vmConfig, sdk.WithProcessRunner(cmd))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(fcSocket)
+
+	err = m.Start(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := m.StopVMM(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	defer func() {
+		if err := m.Shutdown(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}()
+}
