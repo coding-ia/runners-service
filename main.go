ec1d01ffe7aefb2fcfb2ad3d826c455984073701
bjunker <brian.junker@gmail.com>
2024-10-16 09:44:00 -0700

    Update main.go

--- main.go
+++ main.go
@@ -112,7 +112,8 @@ func main() {
 	vmConfig.Seccomp = sdk.SeccompConfig{
 		Enabled: false,
 	}
-	vmConfig.NetNS = "/var/run/netns/551e7604-e35c-42b3-b825-416853441234"
+
+	//vmConfig.NetNS = "/var/run/netns/551e7604-e35c-42b3-b825-416853441234"
 	vmConfig.VMID = "551e7604-e35c-42b3-b825-416853441234"
 
 	logger := log.New()
