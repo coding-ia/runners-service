a630b3382bd6efc3b5668433a0eb6bf64687a9a1
bjunker <brian.junker@gmail.com>
2024-10-17 10:13:10 -0700

    Update main.go

--- main.go
+++ main.go
@@ -146,12 +146,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	idrsaPath := fmt.Sprintf("%s/firecracker/%s", m.Cfg.JailerCfg.ChrootBaseDir, vmConfig.VMID)
-	opensshKey, _ := ssh_gen.GenerateMachineKeys(idrsaPath)
 	err = m.Start(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	idrsaPath := fmt.Sprintf("%s/firecracker/%s/root/idrsa", m.Cfg.JailerCfg.ChrootBaseDir, vmConfig.VMID)
+	opensshKey, _ := ssh_gen.GenerateMachineKeys(idrsaPath)
 
 	metaDataIP := m.Cfg.MmdsAddress.String()
 	fmt.Printf("Metadata IP: %v\n", metaDataIP)
